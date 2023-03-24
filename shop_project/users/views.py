cf12b8c970fffb40296a2a90162cd20f3055ab81
Alexandr <maximchik.alexandr@yandex.ru>
2023-03-24 20:24:21 +0300

    auth jwt

--- shop_project/users/views.py
+++ shop_project/users/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from djoser.views import UserViewSet
+from rest_framework.response import Response
+from django.contrib.auth import get_user_model
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+USER_MODEL = get_user_model()
+
+
+class SomeView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request):
+        return Response("Hello world!")
+
+
+class ActivateUser(UserViewSet):
+    def get_serializer(self, *args, **kwargs):
+        serializer_class = self.get_serializer_class()
+        kwargs.setdefault('context', self.get_serializer_context())
+
+        # this line is the only change from the base implementation.
+        kwargs['data'] = {"uid": self.kwargs['uid'], "token": self.kwargs['token']}
+
+        return serializer_class(*args, **kwargs)
