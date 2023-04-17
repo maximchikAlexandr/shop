89f24e82b2b5a01585ca3b885bad2ca57ba83872
Alexandr <maximchik.alexandr@yandex.ru>
2023-04-17 19:16:50 +0300

    Add new fixtures and renaming in class ClientEndpointsTestCase

--- shop_project/catalog/tests/test_catalog_views.py
+++ shop_project/catalog/tests/test_catalog_views.py
@@ -33,9 +33,9 @@ class ClientEndpointsTestCase(APITestCase):
             for model in fixture:
                 getattr(self, attr_name).append(model["fields"] | {"id": model["pk"]})
 
-    def check_response(self, *, url, fixture, kwargs=None, nested_fixtures=None):
+    def check_response(self, *, url, fixture, kwargs=None, nested_objs=None):
         """Check response
-        nested_fixtures - fixtures for the nested serializers
+        nested_objs - fixtures for the nested serializers
         """
         url = reverse(url, kwargs=kwargs)
         response = self.client.get(url)
@@ -46,50 +46,55 @@ class ClientEndpointsTestCase(APITestCase):
         assert isinstance(response.data, list), \
             f"Type of the data is not list. URL: {url}"
 
-        if nested_fixtures is None:
+        if nested_objs is None:
             self.check_without_nested_serializers(response, url, fixture)
         else:
-            self.check_with_nested_serializers(response, url, fixture, nested_fixtures)
+            self.check_with_nested_serializers(response, url, fixture, nested_objs)
 
     def check_without_nested_serializers(self, response, url, fixture):
         assert response.data == getattr(self, fixture), \
             f"The response data does not match original fixture. URL: {url}"
 
-    def check_with_nested_serializers(self, response, url, fixture, nested_fixtures):
-
-        nested_from_response = {fix: [] for fix in nested_fixtures}
+    def check_with_nested_serializers(self, response, url, fixture, names_of_sub_objs):
+        response_sub_objs = {sub_obj: [] for sub_obj in names_of_sub_objs}
         response_data = [dict(obj) for obj in response.data]
         original_data = getattr(self, fixture)
 
         for orig_obj, resp_obj in zip(original_data, response_data):
             ddiff = dict(DeepDiff(orig_obj, resp_obj, ignore_order=True))
+            response_sub_objs = self.add_response_sub_objs(ddiff, orig_obj, url,
+                                                           names_of_sub_objs,
+                                                           response_sub_objs)
 
-            # ddiff, orig_obj, url
-            for model_name, change in ddiff['type_changes'].items():
-                assert change["old_value"] == change["new_value"]["id"], \
+        self.compare_nested_objects(names_of_sub_objs, response_sub_objs, url)
+
+    @staticmethod
+    def add_response_sub_objs(ddiff, orig_obj, url, names_of_sub_objs, response_sub_objs):
+        for name_obj, changes_in_obj in ddiff['type_changes'].items():
+
+            assert changes_in_obj["new_value"].get("id") is not None, \
+                f"The nested object {name_obj} not contain field 'id'"
+
+            assert changes_in_obj["old_value"] == changes_in_obj["new_value"]["id"], \
                 f"Field 'id' in objects from the nested serializer do not match field 'id' from " \
                 f"the original fixture {orig_obj}. URL: {url}"
 
-                # def get_nested_obj_from_response(nested_fixtures, model, nested_from_response, change)
-                assert len(model_name) > 1
-                assert isinstance(model_name, str)
-                for obj in nested_fixtures:
-                    # assert f"root['{obj.split('_')[1]}']" in ddiff['type_changes']
-                    if model_name == f"root['{obj.split('_')[1]}']":
-                        nested_from_response[obj].append(change["new_value"])
+            for sub_obj in names_of_sub_objs:
+                if name_obj == f"root['{sub_obj.split('_')[1]}']":
+                    response_sub_objs[sub_obj].append(changes_in_obj["new_value"])
 
-        self.compare_nested_objects(nested_fixtures, nested_from_response, url)
+        return response_sub_objs
 
+    def compare_nested_objects(self, names_of_sub_objs, response_sub_objs, url):
+        for sub_obj, changes_in_obj in response_sub_objs.items():
+            assert len(changes_in_obj) > 0, \
+                f"The nested serializer {sub_obj} not work correctly."
 
-    def compare_nested_objects(self, nested_fixtures, nested, url):
-        for x in nested:
-            assert len(x) > 0
-
-        for fix in nested_fixtures:
-            assert len(fix) > 0
-            list_of_differences = [obj for obj in nested[fix] if obj not in getattr(self, fix)]
+        for sub_obj in names_of_sub_objs:
+            fixture = getattr(self, sub_obj)
+            list_of_differences = [obj for obj in response_sub_objs[sub_obj] if obj not in fixture]
             assert list_of_differences == [], \
-                f"A objects from the nested serializer do not match source fixture {fix}. " \
+                f"Objects from the nested serializer do not match source fixture {sub_obj}. " \
                 f"URL: {url}"
 
     def test_categories(self):
@@ -117,7 +122,7 @@ class ClientEndpointsTestCase(APITestCase):
             url="products",
             fixture="catalog_product",
             kwargs=None,
-            nested_fixtures=[
+            nested_objs=[
                 "catalog_discount",
                 "catalog_category",
                 "catalog_producer",
@@ -127,17 +132,32 @@ class ClientEndpointsTestCase(APITestCase):
     # def test_category_products(self):
     #     self.check_response(
     #         url="category_products", fixture="catalog_product",
-    #         kwargs={"category_id": 21}, has_nested_serializers=True
+    #         kwargs={"category_id": 21},
+    #         nested_objs=[
+    #             "catalog_discount",
+    #             "catalog_category",
+    #             "catalog_producer",
+    #         ]
     #     )
     #
-    # def test_producer_products(self):
+    # def test_discount_products(self):
     #     self.check_response(
     #         url="discounts_products", fixture="catalog_product",
-    #         kwargs={"discount_id": 101}, has_nested_serializers=True
+    #         kwargs={"discount_id": 101},
+    #         nested_objs=[
+    #             "catalog_discount",
+    #             "catalog_category",
+    #             "catalog_producer",
+    #         ]
     #     )
     #
     # def test_producer_products(self):
     #     self.check_response(
     #         url="producer_products", fixture="catalog_product",
-    #         kwargs={"producer_id": 87}, has_nested_serializers=True
+    #         kwargs={"producer_id": 87},
+    #         nested_objs=[
+    #             "catalog_discount",
+    #             "catalog_category",
+    #             "catalog_producer",
+    #         ]
     #     )
